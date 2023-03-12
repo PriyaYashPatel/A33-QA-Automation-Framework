57295afe42b017833a008b8ccab99581c63917a7
PriyaYashPatel <priyashpatel30@gmail.com>
2023-03-12 14:14:12 +0530

    Homework17

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -9,7 +9,7 @@ public class Homework17 extends BaseTest {
     @Test
     public static void addSongToPlaylist() throws InterruptedException {
 
-        String newSongAddedNotificationText = "Added 1 song into Test";
+        String newSongAddedNotificationText = "Added 1 song into";
 
         navigateToPage();
         provideEmail("testguest@testpro.io");
@@ -31,7 +31,7 @@ public class Homework17 extends BaseTest {
     }
 
     public static void clickViewAllButton () throws InterruptedException {
-        WebElement viewAllSearchResult = driver.findElement(By.cssSelector("div.results section.songs h1 button")); //  //button[@data-test='view-all-songs-btn']
+        WebElement viewAllSearchResult = driver.findElement(By.cssSelector("div.results section.songs h1 button")); //xpath - //button[@data-test='view-all-songs-btn']
         viewAllSearchResult.click();
         Thread.sleep(3000);
     }
@@ -43,13 +43,12 @@ public class Homework17 extends BaseTest {
     }
 
     public static void clickAddToButton() throws InterruptedException {
-        WebElement addToButton = driver.findElement(By.cssSelector("button.btn-add-to")); ////button[@data-test='add-to-btn']
+        WebElement addToButton = driver.findElement(By.cssSelector("button.btn-add-to")); //xpath - //button[@data-test='add-to-btn']
         addToButton.click();
         Thread.sleep(3000);
     }
 
     public static void choosePlaylist () throws InterruptedException {
-//        Write xPath for playlist where song will be added
         WebElement playlistElement = driver.findElement(By.xpath("//section[@id='songResultsWrapper'] //li[text()[contains(.,'Test123')]]"));
         playlistElement.click();
         Thread.sleep(3000);
